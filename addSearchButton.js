652d5414a03b716bf5edc8c8e1265c54eb84b8d0
craig ballantyne <craigballantyne@gmail.com>
2015-05-11 03:23:04 +0100

    Update addSearchButton.js

--- addSearchButton.js
+++ addSearchButton.js
@@ -13,4 +13,34 @@ if (username != null) {
 	button.value = "Search My Quotes";
 
 	document.querySelector("#navbar_search_menu > table > tbody > tr:nth-child(2) > td > form").appendChild(button);
+
+		
+	if(document.querySelector("#threadsearch_menu") !== null) {
+		var threadButton = document.createElement("input");
+		threadButton.type = "button";
+		threadButton.onclick = function(){
+		    var searchField = document.querySelector("#threadsearch_menu form input.bginput");
+		    searchField.value = "[quote=" + username;
+		    var form = document.querySelector("#threadsearch_menu form");
+		    form.submit();
+		};
+		threadButton.value = "Search My Quotes";
+		
+		document.querySelector("#threadsearch_menu form > table > tbody > tr:nth-child(2) > td").appendChild(threadButton);
+	}
+	if(document.querySelector("#forumsearch_menu") !== null) {
+		var forumButton = document.createElement("input");
+		forumButton.type = "button";
+		forumButton.onclick = function(){
+		    var searchField = document.querySelector("#forumsearch_menu form input.bginput");
+		    searchField.value = "[quote=" + username;
+		    document.querySelector("#rb_fd_sp1").checked = true;
+		    var form = document.querySelector("#forumsearch_menu form");
+		    form.submit();
+		};
+		forumButton.value = "Search My Quotes";
+		
+		document.querySelector("#forumsearch_menu form > table > tbody > tr:nth-child(3) > td").appendChild(forumButton);
+	}
+	
 }
